617c51ff521cab19b0cf6c8b68d1e2f098f813c6
Rachit <rachit.pool147@gmail.com>
2017-05-23 17:42:39 +0530

    inital commit

---
+++ RCTVideo.h
@@ -0,0 +1,14 @@
+#import "RCTView.h"
+#import <AVFoundation/AVFoundation.h>
+#import "AVKit/AVKit.h"
+#import <MediaPlayer/MediaPlayer.h>
+
+@class RCTEventDispatcher;
+
+@interface RCTVideo : UIView
+
+- (instancetype)initWithEventDispatcher:(RCTEventDispatcher *)eventDispatcher NS_DESIGNATED_INITIALIZER;
+
+- (AVPlayerViewController*)createPlayerViewController:(AVPlayer*)player withPlayerItem:(AVPlayerItem*)playerItem;
+
+@end
